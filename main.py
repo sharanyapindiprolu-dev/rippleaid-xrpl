4ec01e7df0fe0566c559aaee1a560a6c4ec4a5e3
sharanyapindiprolu-dev <sharanyapindiprolu@gmail.com>
2026-01-08 16:59:27 +0800

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from xrpl.wallet import Wallet
-
-# Create a new XRP wallet (beneficiary wallet)
-wallet = Wallet.create()
-print(f"Address: {wallet.classic_address}")
-print(f"Secret: {wallet.seed}")
